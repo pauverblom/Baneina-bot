7268f16c3a347129aa89822cb1100477a0d2f2e4
pauverblom <47112988+pauverblom@users.noreply.github.com>
2020-05-18 20:45:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,27 +108,8 @@ if (message.content === '!Unexist'){
       
     }
 if (message.content.startsWith('!avatar') {
-        let Embed = new MessageEmbed()
-        let roles = []
-        if(!message.mentions.users.first()){
-            message.member.roles.cache.forEach(role=>{
-                roles.push(role.name)
-            })
-            Embed.setTitle(`Your avatar!`)
-            Embed.setThumbnail(message.author.displayAvatarURL())
-            Embed.setColor(`RANDOM`)
-            Embed.setDescription(`Joined: (MM/DD/YYYY) ${formatDate(message.member.joinedAt)}\nID: ${message.author.id}\nRoles: ${roles}`)
-            return message.channel.send(Embed)
-        }else{
-            let User = message.mentions.members.first()
-            User.roles.cache.forEach(role=>{
-                roles.push(role.name)
-            })
-            Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s avatar!`)
-            Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL())
-            Embed.setColor(`RANDOM`)
-            Embed.setDescription(`Joined: (MM/DD/YYYY) ${formatDate(User.joinedAt)}\nID: ${User.id}\nRoles: ${roles}`)
-            return message.channel.send(Embed)
+    var user = message.mentions.users.first();
+    message.send(user)
         }
  
   
