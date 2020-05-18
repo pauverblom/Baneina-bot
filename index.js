c14a9a42dff8c7499e9d34ea16c1a93ce8ba963c
pauverblom <47112988+pauverblom@users.noreply.github.com>
2020-05-18 21:04:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const express = require('express');
 const app = express();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
 
 
 
@@ -115,22 +116,6 @@ if (message.content.startsWith('!avatar')) {
         .setImage(user.avatarURL);
     message.channel.send(embed);
         }
-	client.on('message', message => {
-  // If the message is "how to embed"
-  if (message.content === 'how to embed') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-    const embed = new MessageEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xff0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
-  }
  
   
   if (message.attachments.size > 0) {
