dc7d921220ebbc60a21407739134643aae19829f
pauverblom <47112988+pauverblom@users.noreply.github.com>
2020-05-19 10:17:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ if (message.content === '!Unexist'){
 if (message.content.startsWith('!Avatar')) {
     var user = message.mentions.users.first();
     let embed = new Discord.MessageEmbed()
-    .setImage(message.author.AvatarURL)
+    .setImage(message.author.avatarURL)
     .setColor('#275BF0')
     message.channel.send(embed)
 
