7079a72d6712f12982f1b3902912b8d3fd210ff6
pauverblom <47112988+pauverblom@users.noreply.github.com>
2020-11-08 16:10:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -165,13 +165,12 @@ if (message.content.startsWith('!Avatar')) {
 
 
 client.on('guildMemberAdd', member => {
-  var role = member.guild.roles.cache.find(role => role.name === 'Lvl 1 Crook');
-  member.roles.add(role)
- const channel = member.guild.channels.cache.find(ch => ch.name === 'greeting-channel');
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'greeting-channel');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  message.channel.send(`Welcome to the server, ${member} Make sure to subscribe to my channel! (if you aren't already) https://www.youtube.com/c/baneina`);
+  channel.send(`Welcome to the server, ${member}`);
 });	
 
 
