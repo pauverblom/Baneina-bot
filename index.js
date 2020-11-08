bdaa07127acff3b7261fa3067473fd1d728fb2e9
pauverblom <47112988+pauverblom@users.noreply.github.com>
2020-11-08 15:52:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
 	
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'greeting-channel');
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'talk');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
