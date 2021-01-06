374f184235ffa52f3f6045bc02cf8c3c6fc5fd58
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 14:54:45 +0100

    Update exist.js

--- Commands/exist.js
+++ Commands/exist.js
@@ -1,8 +1,8 @@
 module.exports = {
-	name: 'exist',
-	description: 'Do YOU exist?',
-	execute(message, args) {
-		Existence = Existence + 1;
+  name: "exist",
+  description: "Do YOU exist?",
+  execute(message, args) {
+    Existence = Existence + 1;
 
     if (Existence == 1) {
       message.reply("Whoah! You exist! " + Existence + " person exists!");
@@ -11,37 +11,6 @@ module.exports = {
     if (Existence > 1) {
       message.reply("Whoah! You exist! " + Existence + " people exist!");
     }
-  }
-	//!Unexist
-  if (message.content === "!Unexist") {
-    if (Existence > 0) {
-      Existence = Existence - 1;
-    }
-
-    if (Existence == 1) {
-      message.reply(
-        "You commited sewer side! You unexisted! " +
-          Existence +
-          " person exists! " +
-          "Congrats! " +
-          "You are the only survivor!"
-      );
-    }
-    if (Existence > 1) {
-      message.reply(
-        "You commited sewer side! You unexisted! " +
-          Existence +
-          " people exist!"
-      );
-    }
-    if (Existence == 0) {
-      message.reply(
-        "You commited sewer side! " +
-          "You unexisted! " +
-          "No-one exists. " +
-          "This is so sad."
-      );
-    }
-	},
+  },
 };
 
