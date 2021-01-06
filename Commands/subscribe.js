fb74ae55118de483ae81f5065ddd4d16d563e90a
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 15:02:57 +0100

    Create subscribe.js

---
+++ Commands/subscribe.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "ping",
+  description: "Ping!",
+  execute(message, args) {
+    message.reply(
+      "SUBSCRIBE TO MY CHANNEL https://www.youtube.com/channel/UCHDfM2CVCqRR5XhncqNgcuw?sub_confirmation=1"
+    );
+  },
+};
