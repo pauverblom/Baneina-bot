60116f07e3b7e59f7e0e1d5e723ed0a0995ad3bd
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 15:05:32 +0100

    Update subscribe.js

--- Commands/subscribe.js
+++ Commands/subscribe.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: "ping",
-  description: "Ping!",
+  name: "subscribe",
+  description: "Smash Subscribe!",
   execute(message, args) {
     message.reply(
       "SUBSCRIBE TO MY CHANNEL https://www.youtube.com/channel/UCHDfM2CVCqRR5XhncqNgcuw?sub_confirmation=1"
