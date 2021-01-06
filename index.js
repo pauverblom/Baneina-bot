8afd130b167181cf129a73a3d945928a1f94505a
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 15:10:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Discord = require("discord.js");
 const prefix = ("!");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./Commands/${file}`);
