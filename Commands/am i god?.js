1c70d1c408c36fb224e98d8a1e45ad22dff05785
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 15:35:33 +0100

    Create am i god?.js

---
+++ Commands/am i god?.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'am i god?',
+  description: 'Are you?',
+  execute(message, args) {
+    var highestrole = message.member.roles.highest.name;
+    if (highestrole === 'Baneina') {
+      message.reply('Yes, you are, my lord');
+    } else {
+      message.reply("Nahh, bro. You're a loser.");
+    }
+  },
+};
