a50a5b9b1cf912875551b149cc0dddf2d1b78898
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 15:16:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,32 +48,6 @@ client.on("message", (message) => {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
 	}
-	//!Cykablyat
-  if (message.content === "!Cykablyat") {
-    var number2 = 5;
-    var random = Math.floor(Math.random() * 5) + 1;
-    switch (random) {
-      case 1:
-        message.reply("Stay Cheeki Breeki Comrade");
-        break;
-      case 2:
-        message.reply("https://www.youtube.com/watch?v=OUHVRWdVQCI");
-        break;
-      case 3:
-        message.reply("https://www.youtube.com/watch?v=U06jlgpMtQs");
-        break;
-      case 4:
-        message.reply(
-          "https://cdn.discordapp.com/attachments/539112946509545475/554747539279314954/c05b9da.jpg"
-        );
-        break;
-      case 5:
-        message.reply(
-          "https://cdn.discordapp.com/attachments/539112946509545475/554747539279314955/22e6d09.png"
-        );
-        break;
-    }
-  }
 	//!Role
   if (message.content === "!Role") {
     let member = message.member;
