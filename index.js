c692bc9ee3946f1f6321c127456a0b4b53cf8d58
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 18:17:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ if (isNaN(message.content)) {
   if (message.channel.name === 'counting') {
     message.delete();
     message.author.send('The counting channel is exclusively for counting.');
-  } else if (parseInt(message.content, 10) < 780) {
+  } else if (Number(message.content) < 780) {
     console.log('sí');
     if (message.channel.name === 'counting') {
       message.delete();
