ce00b30b9b60e6a58ff24e553bc71ba147efec48
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 17:54:42 +0100

    Create avatar.js

---
+++ Commands/avatar.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: 'avatar',
+  description: 'send the avatar',
+  execute(message, args) {
+    if (!message.mentions.users.size) {
+      const embed1 = new Discord.MessageEmbed().setImage(
+        message.author.displayAvatarURL()
+      );
+      message.channel.send(embed1);
+    } else {
+      var user = message.mentions.users.first();
+      const embed2 = new Discord.MessageEmbed().setImage(
+        user.displayAvatarURL()
+      );
+      message.channel.send(embed2);
+    }
+  },
+};
