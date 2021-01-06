a7fbd2fc720ea6c11c4cde2f969efcae2585f3ea
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-06 14:55:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,49 +32,7 @@ client.on("message", (message) => {
 	
 	const command = args.shift().toLowerCase();
 
-	//!Exist
-  if (message.content === "!Exist") {
-    Existence = Existence + 1;
 
-    if (Existence == 1) {
-      message.reply("Whoah! You exist! " + Existence + " person exists!");
-    }
-
-    if (Existence > 1) {
-      message.reply("Whoah! You exist! " + Existence + " people exist!");
-    }
-  }
-	//!Unexist
-  if (message.content === "!Unexist") {
-    if (Existence > 0) {
-      Existence = Existence - 1;
-    }
-
-    if (Existence == 1) {
-      message.reply(
-        "You commited sewer side! You unexisted! " +
-          Existence +
-          " person exists! " +
-          "Congrats! " +
-          "You are the only survivor!"
-      );
-    }
-    if (Existence > 1) {
-      message.reply(
-        "You commited sewer side! You unexisted! " +
-          Existence +
-          " people exist!"
-      );
-    }
-    if (Existence == 0) {
-      message.reply(
-        "You commited sewer side! " +
-          "You unexisted! " +
-          "No-one exists. " +
-          "This is so sad."
-      );
-    }
-  }
 	//!Meme
   if (message.content === "!Meme") {
     var number1 = 6;
