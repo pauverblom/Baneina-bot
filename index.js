186a26b5241bb58cc8cf28a2e521ca7374fc3953
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-01-09 10:12:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ client.on('guildMemberAdd', (member) => {
 
 client.on('messageReactionAdd', async (reaction, user) => {
 	const channel = user.guild.channels.cache.find((ch) => ch.name === 'rules');
-	let imagememer = user.guild.roles.cache.find((role) => role.name === 'Lvl 1 Crook');
+	let role = message.guild.roles.cache.find((role) => role.name === 'Lvl 1 Crook');
 	if (!channel) return;
 			else {
 	reaction.message.author.roles.add(role);
