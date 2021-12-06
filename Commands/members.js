a5973923fb71225fd65c3b23e917b1fe408ee9af
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-06 12:45:31 +0100

    Update members.js

--- Commands/members.js
+++ Commands/members.js
@@ -2,9 +2,9 @@ module.exports = {
   name: 'members',
   description: 'How many members are there in the server',
   execute(message, args) {
-    var members = message.guild.members.cache.filter(
-      (member) => !member.user.bot
-    ).size;
-    message.reply('There are ' + members + ' members in the server.');
+    var members = message.guild.members.cache.filter((member) => !member.user.bot).size;
+    var members_and_bots = message.guild.members.cache.size;
+    
+    message.reply('There are ' + members_and_bots + ' members in the server, ' + (members_and_bots - members) ' of which are bots');
   },
 };
