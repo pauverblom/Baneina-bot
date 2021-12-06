929cb8baece647b9df4322192078a24b7ccbf7b0
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-06 22:34:28 +0100

    Update mute.js

--- Commands/mute.js
+++ Commands/mute.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'mute',
   description: 'Mute someone you want to mute',
-  execute (message, args) 
+  async run (client, message, args)
   {
     if (message.author.id == 405050037186330624) //me
     {
