dfb93e9bc1309b0fc16471681c8cf1bb65e716af
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-06 22:26:53 +0100

    Update mute.js

--- Commands/mute.js
+++ Commands/mute.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'mute',
   description: 'Mute someone you want to mute',
-  execute(message, args) 
+  async run (client, message, args) 
   {
     if (message.author.id == 405050037186330624) //me
     {
@@ -12,10 +12,28 @@ module.exports = {
       }
       else
       {
+        if (!args[1])
+        return message.channel.send('Please enter a length of time of 14 days or less (1s/m/h/d)');
+        let time = ms(args[1]);
+        if (!time || time > 1209600000) // Cap at 14 days, larger than 24.8 days causes integer overflow
+        return message.channel.send('Please enter a length of time of 14 days or less (1s/m/h/d)');
+
+        let reason = args.slice(2).join(' ');
+        if (!reason) reason = '`None Provided`';
+        if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
+
         var user = message.mentions.members.first();
         let role = message.guild.roles.cache.find((role) => role.id == 917521104908742736);
         user.roles.add(role);
-        message.channel.send(`${user} has been muted.`)
+        message.channel.send(`${user} has been muted indefinitely.`)
+        
+        
+        user.timeout = message.client.setTimeout(async () => {
+          try {
+            await user.roles.remove(role);
+            message.channel.send(`${user} has been unmuted`);
+          }
+        }, time);
       }
     }
   },
