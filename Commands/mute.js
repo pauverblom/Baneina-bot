f5f7cc654c785385f94dbe5f59f56a43a8a557cc
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-06 22:04:03 +0100

    Update mute.js

--- Commands/mute.js
+++ Commands/mute.js
@@ -1,1 +1,22 @@
-
+const Discord = require('discord.js');
+module.exports = {
+  name: 'mute',
+  description: 'Mute someone you want to mute',
+  execute(message, args) 
+  {
+    if (message.author.id == 405050037186330624) //me
+    {
+      if (!message.mentions.users.size)
+      {
+        message.send(`You need to mention someone in order to mute them`);
+      }
+      else
+      {
+        var user = message.mentions.users.first();
+        let role = message.guild.roles.cache.find((role) => role.id == 917521104908742736);
+        user.roles.add(role);
+        message.send(`${user} has been muted.`)
+      }
+    }
+  },
+};
