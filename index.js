d637f50ac6a88c437b1b3f24383927e56fb31503
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-06 14:36:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 const prefix = '!';
+const Swearwords = ["fuck", "shit", "asshole", "cunt"]
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./Commands').filter((file) => file.endsWith('.js'));
@@ -41,10 +42,10 @@ client.on('message', (message) => {
 		message.channel.send(`${message.author}, your brain is flat.`);
 	}
 	
-	if (message.content.toLowerCase().includes('fuck' | 'shit' | 'asshole'))
+	if (BannedWords.some(word => message.toString().toLowerCase().includes(word))) 
 	{
 		message.channel.send(`${message.author}, language.`);
-	}
+	};
 	
         if (isNaN(message.content)) { //
                 if (message.channel.id == 785909768379301940) { //Counting channel
