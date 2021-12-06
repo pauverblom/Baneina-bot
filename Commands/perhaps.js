757a9da0bd0e4a817099e2ab359b14128b417059
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-06 15:19:41 +0100

    Update perhaps.js

--- Commands/perhaps.js
+++ Commands/perhaps.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'perhaps',
   description: 'send perhaps photo',
-  execute(message, args) {
-    if (!message.mentions.users.size) {
-      const embed = new Discord.MessageEmbed().setImage('https://postimg.cc/ctLh0kyF');
-      message.channel.send(embed);
+  execute(message, args) 
+  {
+    const embed = new Discord.MessageEmbed().setImage('https://postimg.cc/ctLh0kyF');
+    message.channel.send(embed);
   },
 };
