816fa171ad815e52e3be62a279e87bc60cd9e222
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-06 23:15:47 +0100

    Update mute.js

--- Commands/mute.js
+++ Commands/mute.js
@@ -15,6 +15,11 @@ module.exports = {
         if (!args[1])
         return message.channel.send('Please enter an amount of time');
         let time = (args[1]);
+        
+        let seconds = time / 1000;
+        let minutes = seconds / 60;
+        let hours = minutes / 60;
+        
         if (!time || time > 1209600000) // Cap at 14 days, larger than 24.8 days causes integer overflow
         return message.channel.send('Please enter an amount of time smaller than 1209600000');
 
@@ -25,13 +30,13 @@ module.exports = {
         var user = message.mentions.members.first();
         let role = message.guild.roles.cache.find((role) => role.id == 917521104908742736);
         user.roles.add(role);
-        message.channel.send(`${user} has been muted for ${(time / 1000)} seconds, or ${(time / 60000)} minutes, or ${(time / 3600000)} hours lol.`);
+        message.channel.send(`${user} has been muted for ${seconds} seconds, or ${minutes.toFixed(4)} minutes, or ${hours.toFixed(3)} hours lol.`);
         
         
         user.timeout = message.client.setTimeout(async () => {
           try {
             await user.roles.remove(role);
-            message.channel.send(`${user} has been unmuted after ${(time / 1000)} seconds, or ${(time / 60000)} minutes, or ${(time / 3600000)} hours lol.`);
+            message.channel.send(`${user} has been unmuted after ${seconds} seconds, or ${minutes.toFixed(4)} minutes, or ${hours.toFixed(3)} hours lol.`);
           } catch (err) {
               console.log(err)
               return message.channel.send('Please check the role hierarchy', err.message);
