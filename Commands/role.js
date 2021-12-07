dc7a25d223582e7f982714338423853bf73d421f
pauverblom <pauver.mola@gmail.com>
2021-12-07 21:17:13 +0100

    Update

--- Commands/role.js
+++ Commands/role.js
@@ -2,8 +2,6 @@ module.exports = {
   name: 'role',
   description: 'Gives your current role',
   execute(message, args) {
-    let member = message.member;
-    var highestrole = member.roles.highest.name;
-    message.reply('Your current role is: ' + highestrole);
+    message.reply('Your current role is: ' + message.member.roles.highest.name);
   },
 };
