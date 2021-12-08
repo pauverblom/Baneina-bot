12c28b965ab3c8dd09d9bb625222dbaf1ff20e92
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-08 10:13:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ client.on('message', (message) => {
 	
 	if (message.member.roles.cache.some(role => role.id == 917521104908742736))
 	{
-	    message.delete();
+		message.author.send("You're muted lol.");
+	    	message.delete();
+		return;
 	}
 	
 	if (message.content.toLowerCase().includes('earth is flat'))
