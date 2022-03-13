1e2571a920f7fa6ad38d9a58cecf9c0bbb62a0bc
pauverblom <47112988+pauverblom@users.noreply.github.com>
2022-03-13 15:38:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ client.on('ready', async client => {
 });
 
 async function statuscheck() {
-    console.log('set'); // /So I know the timer works
+	console.log('set'); // /So I know the timer works
+	return {0};
 }
 
 //Message Sent
