78edb828eb8cfd844f667123225aadda6929fc77
pauverblom <47112988+pauverblom@users.noreply.github.com>
2023-12-26 13:03:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on('ready', () => {
 		type: "WATCHING",
  		url: "https://www.youtube.com/c/baneina"
 });
-        console.log(`Logged in as ${client.user.tag}!`);
+        console.log(`BANEINA DENTRO PUTASSS`);
 	
 	//setInterval(statuscheck, 10000); // runs the check funtion evrey 10s to keep up to date
 });
