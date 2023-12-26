d33b85d81cda28a5db45324c479617e90537cf4b
pauverblom <47112988+pauverblom@users.noreply.github.com>
2023-12-26 20:00:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on('ready', () => {
 		type: "WATCHING",
  		url: "https://www.youtube.com/c/baneina"
 });
-        console.log(`BANEINA DENTROO`);
+        console.log(`BANEINA DENTRO, A ESTUDIAR`);
 	
 	//setInterval(statuscheck, 10000); // runs the check funtion evrey 10s to keep up to date
 });
