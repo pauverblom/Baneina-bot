f0bd8015d45d9da8e5637f0f15ff9b6bb20965b0
pauverblom <pauver,mola@gmail.com>
2023-12-26 00:33:19 +0000

    muerte

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
+ 
+const config = require('./config.json');
+
+const token = config.token;
+
+const client = new Discord.Client({
+partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
 const prefix = '!';
-const swearwords = ["fuck", "shit", "asshole", "cunt", "bitch", "kurwa", "scheiße", "scheisse", "whore"]
-require("dotenv").config();
+const swearwords = ["fuck", "shit", "asshole", "cunt", "bitch", "kurwa", "scheiße", "scheisse", "whore"];
 
 client.commands = new Discord.Collection();
+
 const commandFiles = fs.readdirSync('./Commands').filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles)
@@ -150,4 +158,4 @@ client.on('guildMemberRemove', (member) => {
 	channel.send(embed);
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
