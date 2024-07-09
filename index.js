e308ba6df856ef9d62805fd7f042ac10342baee6
pauverblom <pauver.mola@gmail.com>
2024-07-09 17:27:12 +0200

    Remove express and reformat code
    yea

--- index.js
+++ index.js
@@ -1,158 +1,199 @@
 //Libraries and constants
-const express = require('express');
-const app = express();
-const fs = require('fs');
-const Discord = require('discord.js');
- 
-const config = require('./config.json');
 
+const fs = require("fs");
+const Discord = require("discord.js");
+
+const config = require("./config.json");
 const token = config.token;
 
 const client = new Discord.Client({
 partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
 });
-const prefix = '!';
-const swearwords = ["fuck", "shit", "asshole", "cunt", "bitch", "kurwa", "scheiße", "scheisse", "whore"];
-const hardswearwords = ["nigga","nigger", "fag", "faggot","niggas","niggers","n!gga","n!gger","n!ggas","n!ggers","n8gger","n8gga","n8ggas","n8ggers"];
+const prefix = "!";
+const swearwords = [
+  "fuck",
+  "shit",
+  "asshole",
+  "cunt",
+  "bitch",
+  "kurwa",
+  "scheiße",
+  "scheisse",
+  "whore"
+];
+const hardswearwords = [
+  "nigga",
+  "nigger",
+  "fag",
+  "faggot",
+  "niggas",
+  "niggers",
+  "n!gga",
+  "n!gger",
+  "n!ggas",
+  "n!ggers",
+  "n8gger",
+  "n8gga",
+  "n8ggas",
+  "n8ggers"
+];
+
+const baneinaID = 405050037186330624;
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./Commands').filter((file) => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./Commands")
+  .filter((file) => file.endsWith(".js"));
 
-for (const file of commandFiles)
-{
-	const command = require(`./Commands/${file}`);
-	client.commands.set(command.name, command);
+for (const file of commandFiles) {
+  const command = require(`./Commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 //declare useful functions
 
-function attachIsImage(msgAttach)
-{
-	var url = msgAttach.url;
-	return (url.indexOf('jpg' | 'png' | 'jpeg', url.length - ('jpg' | 'png' | 'jpeg').length) !== -1); //return true if it's an image
+function attachIsImage(msgAttach) {
+  var url = msgAttach.url;
+  return (
+    url.indexOf(
+      "jpg" | "png" | "jpeg",
+      url.length - ("jpg" | "png" | "jpeg").length
+    ) !== -1
+  ); //return true if it's an image
 }
 
+client.on("ready", () => {
+  client.user.setActivity("Help me", {
+    type: "WATCHING",
+    url: "https://www.youtube.com/c/baneina"
+  });
 
-client.on('ready', () => {
-	client.user.setActivity("Help me", {
-		type: "WATCHING",
- 		url: "https://www.youtube.com/c/baneina"
-	});
-
-	const testingbotchannel = client.channels.cache.get('559312894815240202');
-	testingbotchannel.send(`BANEINA'S SLAVE UPDATED`);
-	
+  const testingbotchannel = client.channels.cache.get("559312894815240202");
+  testingbotchannel.send(`BANEINA'S SLAVE UPDATED`);
 });
 
+client.on("message", (message) => {
+  if (message.guild === null) {
+    return;
+  }
+  if (message.channel.id == 881158694396633108) {
+    if (!message.content.toLowerCase().includes("ñ")) {
+      message.delete();
+      message.author.send(
+        "The #ñ channel is exclusively for ñ. DO NOT DO THAT AGAIN OR I WILL FIND YOU"
+      );
+    }
+  }
 
-client.on('message', (message) => {
+  if (
+    message.content.toLowerCase().includes("earth is flat") ||
+    message.content.toLowerCase().includes("earth's flat")
+  ) {
+    message.channel.send(`${message.author}, your brain is flat.`);
+  }
 
-	if (message.guild === null)
-	{
-		return;
-	}
-	if (message.channel.id == 881158694396633108)
-	{
-		if (!message.content.toLowerCase().includes('ñ'))
-		{
-			message.delete();
-			message.author.send('The #ñ channel is exclusively for ñ. DO NOT DO THAT AGAIN OR I WILL FIND YOU');
-		}
-	}
+  if (
+    swearwords.some((word) => message.toString().toLowerCase().includes(word))
+  ) {
+    let admin_role = message.guild.roles.cache.find(
+      (role) => role.id == 759034196235911210
+    ); //admin
+    if (message.member.roles.highest.position < admin_role.position) {
+      message.channel.send(`${message.author}, language.`);
+    }
+  }
 
-	if (message.content.toLowerCase().includes('earth is flat') || message.content.toLowerCase().includes("earth's flat"))
-	{
-		message.channel.send(`${message.author}, your brain is flat.`);
-	}
+  if (
+    hardswearwords.some((word) =>
+      message.toString().toLowerCase().includes(word)
+    )
+  ) {
+    message.author.send(
+      `${message.author} that word is not permitted. You will be banned if you continue using it.`
+    );
+    message.delete();
+  }
 
-	if (swearwords.some(word => message.toString().toLowerCase().includes(word)))
-	{
-		let admin_role = message.guild.roles.cache.find(role => role.id == 759034196235911210); //admin
-   			if (message.member.roles.highest.position < admin_role.position)
-		{
-			message.channel.send(`${message.author}, language.`);
-		}
-	};
+  if (isNaN(message.content)) {
+    //
+    if (message.channel.id == 785909768379301940) {
+      //Counting channel
+      message.delete();
+      message.author.send(
+        "The counting channel is exclusively for counting. Do not do that again, I am living in your walls."
+      );
+    }
+  }
 
-	if (hardswearwords.some(word => message.toString().toLowerCase().includes(word)))
-	{
-		message.author.send(`${message.author} that word is not permitted. You will be banned if you continue using it.`);
-		message.delete();
-	};
+  if (Number(message.content) < 2500) {
+    if (message.channel.id == 785909768379301940) {
+      //Counting channel
+      message.delete();
+    }
+  }
 
-        if (isNaN(message.content)) { //
-                if (message.channel.id == 785909768379301940) { //Counting channel
-                        message.delete();
-                        message.author.send('The counting channel is exclusively for counting. Do not do that again, I am living in your walls.');
-                }
-        }
+  //Give image memer role
+  if (message.attachments.size > 0) {
+    if (message.attachments.every(attachIsImage)) {
+      let imagememer = message.guild.roles.cache.find(
+        (role) => role.id == 559311593020588032
+      );
+      message.member.roles.add(imagememer);
+    }
+  }
 
-        if (Number(message.content) < 2500) {
-                if (message.channel.id == 785909768379301940) { //Counting channel
-                        message.delete();
-                }
-        }
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (!client.commands.has(command)) return;
 
-        //Give image memer role
-        if (message.attachments.size > 0) {
-                if (message.attachments.every(attachIsImage))
-		{
-                	let imagememer = message.guild.roles.cache.find((role) => role.id == 559311593020588032);
-                  	message.member.roles.add(imagememer);
-                }
-        }
+    try {
+      client.commands.get(command).execute(message, args);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-        if (message.content.startsWith(prefix))
-	{
-		const args = message.content.slice(prefix.length).trim().split(/ +/);
-		const command = args.shift().toLowerCase();
-		if (!client.commands.has(command)) return;
-
-		try
-		{
-			client.commands.get(command).execute(message, args);
-                }
-		catch (error)
-		{
-                 console.error(error);
-             	}
-        }
-
-	if (message.content.toLowerCase() === 'am i god?')
-	{
-		if (message.author.id == 405050037186330624)
-		{
-			message.channel.send('Yes, you are, my lord');
-		}
-		else
-		{
-			message.channel.send('Step aside, peasant');
-		}
-	}
+  if (message.content.toLowerCase() === "am i god?") {
+    if (message.author.id == baneinaID) {
+      message.channel.send("Yes, you are, my lord");
+    } else {
+      message.channel.send("Step aside, peasant");
+    }
+  }
 });
 
 //Welcoming
-client.on('guildMemberAdd', (member) => {
-	const newbie_role = member.guild.roles.cache.find((role) => role.id == 539113230581104644);
-  const channel = member.guild.channels.cache.find((ch) => ch.id == 552852895704547328); //welcoming channel
+client.on("guildMemberAdd", (member) => {
+  const newbie_role = member.guild.roles.cache.find(
+    (role) => role.id == 539113230581104644
+  );
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.id == 552852895704547328
+  ); //welcoming channel
 
-	channel.send(`Welcome to the server, ${member}. Accept the rules to gain access to the server, and subscribe to my channel! https://www.youtube.com/c/baneina`);
-	member.roles.add(newbie_role);
+  channel.send(
+    `Welcome to the server, ${member}. Accept the rules to gain access to the server, and subscribe to my channel! https://www.youtube.com/c/baneina`
+  );
+  member.roles.add(newbie_role);
 });
 
-
-
-client.on('guildMemberRemove', (member) => {
-	const channel = member.guild.channels.cache.find((channel) => channel.id == 782674407713144842); //dock of shame
+client.on("guildMemberRemove", (member) => {
+  const channel = member.guild.channels.cache.find(
+    (channel) => channel.id == 782674407713144842
+  ); //dock of shame
   const embed = new Discord.MessageEmbed()
-  	.setColor('#FF0000')
-  	.setTitle('Dock of Shame')
-  	.setDescription(`${member.user.tag} has been eliminated and sent to the dock of shame`)
-  	.setImage('https://i.postimg.cc/qMB7Vxnv/Dock-of-shame.jpg');
-		
-	channel.send(embed);
+    .setColor("#FF0000")
+    .setTitle("Dock of Shame")
+    .setDescription(
+      `${member.user.tag} has been eliminated and sent to the dock of shame. Coward.`
+    )
+    .setImage("https://i.postimg.cc/qMB7Vxnv/Dock-of-shame.jpg");
+
+  channel.send(embed);
 });
 
 client.login(token);
